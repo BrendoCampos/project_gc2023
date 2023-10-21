385ad4cfa09bf2552af0095c0e68fec12246a82d
Brendo Campos <bcss@icomp.ufam.edu.br>
2023-10-20 20:03:07 -0400

    Primeiro commit

---
+++ src/main/java/com/project_gc2023/App.java
@@ -0,0 +1,22 @@
+package com.project_gc2023;
+
+public class App {
+    public double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+    }
+
+    public double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+    public static void main(String[] args) {
+        App converter = new App();
+
+        double fahrenheit = 32.0;
+        double celsius = converter.fahrenheitToCelsius(fahrenheit);
+        System.out.println(fahrenheit + " Fahrenheit = " + celsius + " Celsius");
+
+        double celsius2 = 100.0;
+        double fahrenheit2 = converter.celsiusToFahrenheit(celsius2);
+        System.out.println(celsius2 + " Celsius = " + fahrenheit2 + " Fahrenheit");
+    }
+}
---
+++ src/test/java/com/project_gc2023/AppTest.java
@@ -0,0 +1,29 @@
+package com.project_gc2023;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class AppTest {
+    @Test
+    public void testFahrenheitToCelsius() {
+        App converter = new App();
+        double result = converter.fahrenheitToCelsius(32.0);
+        assertEquals(0.0, result, 0.001);
+    }
+
+    @Test
+    public void testFahrenheitToCelsiusNegative() {
+        App converter = new App();
+        double result = converter.fahrenheitToCelsius(-40.0);
+        assertEquals(-40.0, result, 0.001);
+    }
+
+    @Test
+    public void testFahrenheitToCelsiusPositive() {
+        App converter = new App();
+        double result = converter.fahrenheitToCelsius(212.0);
+        assertEquals(100.0, result, 0.001);
+    }
+}
